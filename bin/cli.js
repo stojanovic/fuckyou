2b3552a75112bcc236eaa42ea051e96f120b133e
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2015-08-16 20:21:55 +0200

    feat(cli): add more options for cli
    now it's possible to call 'fuck you|off|u process', 'fuck process' or even 'fu process'.

--- bin/cli.js
+++ bin/cli.js
@@ -6,12 +6,20 @@
 var argv = require('minimist')(process.argv.slice(2)),
     fuck = require('../fuck');
 
-if (argv._[0] === 'you') {
-  fuck(argv._[1]).then(function (response) {
+var args = ['you', 'off', 'u'];
+
+var fuckYou = function fuckYou(processName) {
+  fuck(processName).then(function (response) {
     return console.log(response);
   })['catch'](function (err) {
     return console.error(err);
   });
+};
+
+if (argv._.length > 1 && args.indexOf(argv._[0]) > -1) {
+  fuckYou(argv._[1]);
+} else if (argv._.length === 1) {
+  fuckYou(argv._[0]);
 } else {
   console.log('help');
 }
--- src/cli.js
+++ src/cli.js
@@ -5,10 +5,18 @@
 const argv = require('minimist')(process.argv.slice(2))
     , fuck = require('../fuck')
 
-if (argv._[0] === 'you') {
-  fuck(argv._[1])
+let args = ['you', 'off', 'u']
+
+let fuckYou = function(processName) {
+  fuck(processName)
     .then(response => console.log(response))
     .catch(err => console.error(err))
+}
+
+if (argv._.length > 1 && args.indexOf(argv._[0]) > -1) {
+  fuckYou(argv._[1])
+} else if (argv._.length === 1) {
+  fuckYou(argv._[0])
 } else {
   console.log('help')
 }
